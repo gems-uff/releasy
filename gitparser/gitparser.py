dd3df1be78d138267d4582c7d89859a74e11518e
Felipe Curty <felipecrp@gmail.com>
2018-03-29 00:45:28 -0300

    add commit message #1

--- gitparser/gitparser.py
+++ gitparser/gitparser.py
@@ -2,39 +2,41 @@ import os
 import subprocess
 
 # inspired on http://blog.lost-theory.org/post/how-to-parse-git-log-output/
-GIT_FORMAT = ['%H', '%P', '%cI']
+GIT_FORMAT = ['%H', '%P', '%s', '%cI']
 GIT_FORMAT = '%x1f'.join(GIT_FORMAT) + '%x1e'
 
 class History():
     def __init__(self):
         self.history = dict()
 
-    def commit(self, commit_hash, parent_hashes, commit_date):
+    def commit(self, commit_hash, parent_hashes, commit_message, commit_date):
         parent_commit = list()
         for parent_hash in parent_hashes:
             parent_commit.append(self.history[parent_hash])
 
-        commit = Commit(commit_hash, parent_commit, commit_date)
+        commit = Commit(commit_hash, parent_commit, commit_message, commit_date)
         self.history[commit_hash] = commit
 
 class Commit():
-    def __init__(self, hash, parent, date):
+    def __init__(self, hash, parent, message, date):
         self.hash = hash
         self.parent = parent
+        self.message = message
         self.date = date
 
 def main():
     # adapted from https://stackoverflow.com/questions/2715847/python-read-streaming-input-from-subprocess-communicate/17698359#17698359
     log = subprocess.Popen('git log --reverse --format="%s"' % GIT_FORMAT,
                            stdout=subprocess.PIPE, bufsize=1)
-    
+
     history = History()
     with log.stdout:
         for log_record in iter(log.stdout.readline, b''):
             log_info = log_record.split(b'\x1f')
             commit_hash = log_info[0]
             parent_hash = log_info[1].split()
-            commit_date = log_info[2]
-            history.commit(commit_hash, parent_hash, commit_date)
+            commit_message = log_info[2]
+            commit_date = log_info[3]
+            history.commit(commit_hash, parent_hash, commit_message, commit_date)
 
 main()
