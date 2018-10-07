0edd13581f1d5acfbd0ed7def5d9d94fef3649f4
Felipe Curty <felipecrp@gmail.com>
2018-10-07 13:11:34 -0300

    Chenge setup version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='releasy-uff',
-    version='1.0.3',
+    version='1.1.0',
     author="Felipe Curty",
     author_email="felipecrp@gmail.com",
     description="Releasy is a tool that collects provenance data from releases by parsing the software version control and issue tracking systems.",
