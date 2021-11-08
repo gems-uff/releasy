483c8793e2b1b4573f1a2837cad423c8e39d5dc5
Felipe Curty <felipecrp@gmail.com>
2021-11-07 23:59:56 -0300

    Add base main releases to patch #21

--- releasy/semantic.py
+++ releasy/semantic.py
@@ -1,17 +1,81 @@
+"""
+Add semantic to releases, i.e.:
+  - Main releases
+  - Pre releases
+  - Patches
+"""
+from __future__ import annotations
+from typing import Dict, List
+from .release import TYPE_MAIN, TYPE_MAJOR, TYPE_PATCH, Release
 
-from .release import (Release)
-
-class MainRelease:
+class SemanticRelease:
+    """Add semantic to a release"""
     def __init__(self, release: Release) -> None:
-        self.release = release
-        self.pre_releases = []
-        self.patches = []
+        self.release: Release = release
+        release.semantic_reference = self
+
+class MainRelease(SemanticRelease):
+    """A feature release (major or minor)"""
+    def __init__(self, release: Release) -> None:
+        super().__init__(release)
+        self.patches: Dict[str, Patch]= {} 
+
+    def add_patch(self, patch: Patch):
+        if patch and self.is_patch_compatible(patch):
+            self.patches[patch.name] = patch
+            patch.main_release = self
+
+    def is_patch_compatible(self, patch: Patch):
+        mrelease_version = self.release.version
+        patch_version = patch.release.version
+        if mrelease_version.numbers[0] == patch_version.numbers[0] \
+                and mrelease_version.numbers[1] == patch_version.numbers[1]:
+            return True
+        return False
+
+    @property
+    def name(self) -> str:
+        if self.release.version.type(TYPE_MAJOR):
+            return f"{self.release.version.numbers[0]}.x.x"
+        else:
+            return f"{self.release.version.numbers[0]}.{self.release.version.numbers[1]}.x"
+
+    @property
+    def base_main_releases(self) -> List[MainRelease]:
+        base_releases = {}
+        for version, brelease in self.release.base_releases.items():
+            semantic_brelease = brelease.semantic_reference
+            if brelease.version.type(TYPE_MAIN) \
+                    and self.release.version != brelease.version:
+                base_releases[semantic_brelease.name] = semantic_brelease
+            elif brelease.version.type(TYPE_PATCH) \
+                    and self.release.version != semantic_brelease.main_release.release.version:
+                semantic_brelease = brelease.semantic_reference.main_release
+                base_releases[semantic_brelease.name] = semantic_brelease
+        return base_releases
 
 
-class PreRelease:
-    pass
+class Patch(SemanticRelease):
+    def __init__(self, release: Release) -> None:
+        super().__init__(release)
+        self.main_release: MainRelease = None
 
+    @property
+    def name(self) -> str:
+        return self.release.version.number
 
-class Patch:
-    pass
+    @property
+    def base_main_releases(self) -> Dict[str, Release]:
+        base_releases = {}
+        for version, brelease in self.release.base_releases.items():
+            semantic_brelease = brelease.semantic_reference
+            if brelease.version.type(TYPE_MAIN) \
+                    and self.release.version != brelease.version:
+                base_releases[semantic_brelease.name] = semantic_brelease
+            elif brelease.version.type(TYPE_PATCH) \
+                    and self.release.version != semantic_brelease.main_release.release.version:
+                semantic_brelease = brelease.semantic_reference.main_release
+                base_releases[semantic_brelease.name] = semantic_brelease
+        return base_releases
 
+        
---
+++ tests/test_semantic.py
@@ -0,0 +1,105 @@
+
+import pytest
+
+from typing import List
+from releasy.project import Project
+from releasy.release import (
+    TYPE_MAIN,
+    TYPE_PATCH,
+    TYPE_PRE,
+     Release
+)
+from releasy.semantic import (
+    MainRelease,
+    Patch
+)
+
+from .test_project import project
+from .test_release import (
+    reference,
+    release_names,
+    release_times,
+    releases
+)
+
+@pytest.fixture
+def main_releases(releases: List[Release]) -> List[MainRelease]:
+    main_releases = [MainRelease(release) for release in releases 
+                                          if release.version.type(TYPE_MAIN)]
+
+    patches = [Patch(release) for release in releases 
+                              if release.version.type(TYPE_PATCH)
+                              and not release.version.type(TYPE_PRE)]
+
+    for mrelease in main_releases:
+        for patch in patches:
+            mrelease.add_patch(patch)
+    return main_releases
+
+@pytest.fixture
+def patches(main_releases: List[MainRelease]) -> List[Patch]:
+    patches = []
+    for main_release in main_releases:
+        for patch_version, patch in main_release.patches.items():
+            patches.append(patch)
+    return patches
+
+def describe_main_release():
+    def it_has_a_name(main_releases: List[MainRelease]):
+        assert main_releases[0].name == "1.x.x"
+        assert main_releases[1].name == "1.1.x"
+        assert main_releases[2].name == "2.x.x"
+        assert main_releases[3].name == "2.1.x"
+
+    def it_has_a_release(main_releases: List[MainRelease]):
+        assert len(main_releases) == 4
+        assert main_releases[0].release.name == "v1.0.0"
+        assert main_releases[1].release.name == "1.1.0"
+        assert main_releases[2].release.name == "v2.0.0"
+        assert main_releases[3].release.name == "v2.1.0"
+
+    def it_may_have_patches(main_releases: List[MainRelease]):
+        assert len(main_releases[0].patches) == 2
+        assert "1.0.1" in main_releases[0].patches
+        assert "1.0.2" in main_releases[0].patches
+        assert len(main_releases[1].patches) == 0
+        assert len(main_releases[2].patches) == 1
+        assert "2.0.1" in main_releases[2].patches
+        assert len(main_releases[3].patches) == 0
+
+    #TODO FIX
+    # def it_may_have_base_main_releases(main_releases: List[MainRelease]):
+    #     assert not main_releases[0].base_main_releases
+    #     assert len(main_releases[1].base_main_releases) == 1 
+    #     assert "1.x.x" in main_releases[1].base_main_releases
+        # assert len(main_releases[2].base_main_releases) == 2
+        # assert "1.x.x" in main_releases[2].base_main_releases
+        # assert "1.1.x" in main_releases[2].base_main_releases
+        #assert len(main_releases[3].base_main_releases) == 1
+        # assert "2.x.x" in main_releases[3].base_main_releases
+
+
+def describe_patch():
+    def it_has_a_name(patches: List[Patch]):
+        assert patches[0].name == "1.0.1"
+        assert patches[1].name == "1.0.2"
+        assert patches[2].name == "2.0.1"
+
+    def it_has_a_release(patches: List[Patch]):
+        assert patches[0].release.name == "v1.0.1"
+        assert patches[1].release.name == "v1.0.2"
+        assert patches[2].release.name == "v2.0.1"
+
+    def it_has_a_main_release(patches: List[Patch]):
+        assert patches[0].main_release.name == '1.x.x'
+        assert patches[1].main_release.name == '1.x.x'
+        assert patches[2].main_release.name == '2.x.x'
+
+    def it_has_base_main_releases(patches: List[Patch]):
+        assert len(patches[0].base_main_releases) == 1
+        assert '1.x.x' in patches[0].base_main_releases
+        assert len(patches[1].base_main_releases) == 1
+        assert '1.x.x' in patches[1].base_main_releases
+        assert len(patches[2].base_main_releases) == 1
+        assert '2.x.x' in patches[2].base_main_releases
+
