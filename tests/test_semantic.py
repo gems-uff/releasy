105e6e592c8bc1db540b81d904538e4303857f8b
Felipe Curty <felipecrp@gmail.com>
2021-12-09 15:17:32 -0300

    Add test to commits

--- tests/test_semantic.py
+++ tests/test_semantic.py
@@ -103,6 +103,21 @@ def describe_main_release():
         r200.add_base_release(r100)
         r110.add_base_release(r100)
 
+    def it_has_commits(main_releases: SmReleaseSet):
+        assert main_releases['v1.0.0'].commits == \
+            set([Commit(1), Commit(0)])
+        assert main_releases['1.1.0'].commits == \
+            set([Commit(6), Commit(5), Commit(2)])
+        assert main_releases['v2.0.0'].commits == \
+            set([Commit(14), Commit(12), Commit(11), Commit(10), Commit(9),
+            Commit(8), Commit(7), Commit(4)])
+        assert main_releases['v2.1.0'].commits == \
+            set([Commit(20), Commit(19), Commit(18), Commit(17), Commit(16),
+                 Commit(15)])
+        # assert main_releases['1.1.0'].delay == datetime.timedelta(days=4, hours=23)
+        # assert main_releases['v2.0.0'].delay == datetime.timedelta(days=8, hours=1)
+        # assert main_releases['v2.1.0'].delay == datetime.timedelta(days=5, hours=23)
+
 def describe_patch():
     def it_has_a_name(patches: SmReleaseSet):
         assert patches['v1.0.1'].name == 'v1.0.1'
@@ -119,8 +134,18 @@ def describe_patch():
         assert patches['v1.0.2'].main_release.name == 'v1.0.0'
         assert patches['v2.0.1'].main_release.name == 'v2.0.0'
 
+    def it_has_commits(patches: SmReleaseSet):
+        assert patches['v1.0.1'].commits == patches['v1.0.1'].release.commits
+        assert patches['v1.0.2'].commits == patches['v1.0.2'].release.commits
+        assert patches['v2.0.1'].commits == patches['v2.0.1'].release.commits
 
 def describe_pre_release():
     def it_has_a_name(pre_releases: SmReleaseSet):
         assert pre_releases["v2.0.0-alpha1"].name == "v2.0.0-alpha1"
         assert pre_releases["v2.0.0-beta1"].name == "v2.0.0-beta1"
+
+    def it_has_commits(pre_releases: SmReleaseSet):
+        assert pre_releases["v2.0.0-alpha1"].commits == \
+            pre_releases["v2.0.0-alpha1"].release.commits
+        assert pre_releases["v2.0.0-beta1"].commits == \
+            pre_releases["v2.0.0-beta1"].release.commits
