487854160f50bbe6e144fe2726d80d4c2a263b34
Felipe Curty <felipecrp@gmail.com>
2022-06-08 08:39:48 -0300

    Add more tests to semantic releases

--- tests/test_miner_semantic.py
+++ tests/test_miner_semantic.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 import releasy
 from releasy.miner_release import FinalReleaseMiner
-from releasy.miner_commit import MixedHistoryCommitMiner
+from releasy.miner_commit import HistoryCommitMiner, MixedHistoryCommitMiner
 from releasy.miner_base_release import BaseReleaseMiner
 from releasy.project import Project
 from releasy.miner_semantic import SemanticReleaseMiner
@@ -12,47 +12,136 @@ from releasy.semantic import MainRelease, Patch, SReleaseSet
 
 from .mock_repository import MockRepository
 
-@pytest.fixture
-def project() -> Project:
-    project = releasy.Miner(MockRepository()).apply(
-        FinalReleaseMiner(),
-        MixedHistoryCommitMiner(),
-        BaseReleaseMiner(),
-        SemanticReleaseMiner()
-    ).mine()
-    return project
-
-@pytest.fixture
-def mreleases(project: Project):
-    return project.main_releases
-
-@pytest.fixture
-def patches(project: Project):
-    return project.patches
 
 class describe_release_miner:
-    def it_mine_main_releases(self, mreleases: SReleaseSet[MainRelease]):
+    @pytest.fixture(autouse=True)
+    def project(self) -> Project:
+        project = releasy.Miner(MockRepository()).apply(
+            FinalReleaseMiner(),
+            HistoryCommitMiner(),
+            BaseReleaseMiner(),
+            SemanticReleaseMiner()
+        ).mine()
+        self.project = project
+
+    def it_mine_main_releases(self):
+        mreleases = self.project.main_releases
         assert len(mreleases) == 4
         assert mreleases['0.9.0'].releases.names == set(['v0.9.0'])
         assert mreleases['1.0.0'].releases.names == set(['v1.0.0'])
         assert mreleases['1.1.0'].releases.names == set(['1.1.0'])
         assert mreleases['2.0.0'].releases.names == set(['v2.0.0', '2.0'])
 
-    def it_mine_main_release_patches(self, mreleases: SReleaseSet[MainRelease]):
+    def it_mine_main_release_patches(self):
+        mreleases = self.project.main_releases
         assert not mreleases['0.9.0'].patches
         assert mreleases['1.0.0'].patches.names == set(['1.0.2'])
         assert mreleases['1.1.0'].patches.names == set(['1.1.1'])
         assert mreleases['2.0.0'].patches.names == set(['2.0.1'])
 
-    def it_mine_patches(self, patches: SReleaseSet[Patch]):
+    def it_mine_patches(self):
+        patches = self.project.patches
         assert len(patches) == 4
         assert patches['1.0.2'].releases.names == set(['v1.0.2'])
         assert patches['1.1.1'].releases.names == set(['1.1.1'])
         assert patches['2.0.1'].releases.names == set(['v2.0.1'])
         assert patches['2.1.1'].releases.names == set(['v2.1.1'])
 
-    def it_mine_main_release_commits(self, project: Project, mreleases: SReleaseSet[MainRelease]):
-        repo = project.repository
+    def it_mine_main_release_commits(self):
+        mreleases = self.project.main_releases
+        repo = self.project.repository
+        assert mreleases['0.9.0'].commits.ids == set(['1', '0'])
+        assert mreleases['1.0.0'].commits.ids == set(['3', '2'])
+        assert mreleases['1.1.0'].commits.ids == set(['6', '5'])
+        assert mreleases['2.0.0'].commits.ids \
+            == set(['15', '14', '12', '11', '10', '9', '8']) 
+
+    def it_mine_patches_commits(self):
+        patches = self.project.patches
+        repo = self.project.repository
+        assert patches['1.0.2'].commits.ids == set(['13'])
+        assert patches['1.1.1'].commits.ids == set(['7', '4'])
+        assert patches['2.0.1'].commits.ids == set(['14'])
+        assert patches['2.1.1'].commits.ids == \
+            set(['20', '19', '17', '18', '16'])
+
+    def it_mine_base_mreleases(self):
+        mreleases = self.project.main_releases
+        assert not mreleases['0.9.0'].base_mreleases.names
+        assert mreleases['1.0.0'].base_mreleases.names == set(['0.9.0'])
+        assert mreleases['1.1.0'].base_mreleases.names == set(['1.0.0'])
+        assert mreleases['2.0.0'].base_mreleases.names == set(['1.0.0', '1.1.0'])
+
+    def it_mine_main_base_mrelease(self):
+        mreleases = self.project.main_releases
+        assert not mreleases['0.9.0'].base_mrelease
+        assert mreleases['1.0.0'].base_mrelease.name == '0.9.0'
+        assert mreleases['1.1.0'].base_mrelease.name == '1.0.0'
+        assert mreleases['2.0.0'].base_mrelease.name == '1.1.0'
+
+    def it_mine_main_release_time(self):
+        mreleases = self.project.main_releases
+        assert mreleases['0.9.0'].time \
+            == MockRepository.ref_dt + timedelta(days=1)
+        assert mreleases['1.0.0'].time \
+            == MockRepository.ref_dt + timedelta(days=3)
+        assert mreleases['1.1.0'].time \
+            == MockRepository.ref_dt + timedelta(days=6)
+        assert mreleases['2.0.0'].time \
+            == MockRepository.ref_dt + timedelta(days=14)
+
+    def it_mine_main_release_cycle(self):
+        mreleases = self.project.main_releases
+        assert mreleases['0.9.0'].cycle == None
+        assert mreleases['1.0.0'].cycle == timedelta(days=2)
+        assert mreleases['1.1.0'].cycle == timedelta(days=3)
+        assert mreleases['2.0.0'].cycle == timedelta(days=8)
+
+    def it_mine_main_release_delay(self):
+        mreleases = self.project.main_releases
+        assert mreleases['0.9.0'].delay == timedelta(0)
+        assert mreleases['1.0.0'].delay == timedelta(0)
+        assert mreleases['1.1.0'].delay == timedelta(0)
+        assert mreleases['2.0.0'].delay == timedelta(0)
+
+
+class describe_release_miner_mixed:
+    @pytest.fixture(autouse=True)
+    def project(self) -> Project:
+        project = releasy.Miner(MockRepository()).apply(
+            FinalReleaseMiner(),
+            MixedHistoryCommitMiner(),
+            BaseReleaseMiner(),
+            SemanticReleaseMiner()
+        ).mine()
+        self.project = project
+
+    def it_mine_main_releases(self):
+        mreleases = self.project.main_releases
+        assert len(mreleases) == 4
+        assert mreleases['0.9.0'].releases.names == set(['v0.9.0'])
+        assert mreleases['1.0.0'].releases.names == set(['v1.0.0'])
+        assert mreleases['1.1.0'].releases.names == set(['1.1.0'])
+        assert mreleases['2.0.0'].releases.names == set(['v2.0.0', '2.0'])
+
+    def it_mine_main_release_patches(self):
+        mreleases = self.project.main_releases
+        assert not mreleases['0.9.0'].patches
+        assert mreleases['1.0.0'].patches.names == set(['1.0.2'])
+        assert mreleases['1.1.0'].patches.names == set(['1.1.1'])
+        assert mreleases['2.0.0'].patches.names == set(['2.0.1'])
+
+    def it_mine_patches(self):
+        patches = self.project.patches
+        assert len(patches) == 4
+        assert patches['1.0.2'].releases.names == set(['v1.0.2'])
+        assert patches['1.1.1'].releases.names == set(['1.1.1'])
+        assert patches['2.0.1'].releases.names == set(['v2.0.1'])
+        assert patches['2.1.1'].releases.names == set(['v2.1.1'])
+
+    def it_mine_main_release_commits(self):
+        mreleases = self.project.main_releases
+        repo = self.project.repository
         assert mreleases['0.9.0'].commits \
             == set([Commit(repo, '0'), Commit(repo, '1')])
         assert mreleases['1.0.0'].commits \
@@ -64,8 +153,9 @@ class describe_release_miner:
                     Commit(repo, '2'), Commit(repo, '11'), Commit(repo, '12'), 
                     Commit(repo, '14'),  Commit(repo, '15')])
 
-    def it_mine_patches_commits(self, project: Project, patches: SReleaseSet[Patch]):
-        repo = project.repository
+    def it_mine_patches_commits(self):
+        patches = self.project.patches
+        repo = self.project.repository
         assert patches['1.0.2'].commits == set([Commit(repo, '13')])
         assert patches['1.1.1'].commits \
             == set([Commit(repo, '4'), Commit(repo, '7')])
@@ -78,20 +168,23 @@ class describe_release_miner:
                     Commit(repo, '18'), Commit(repo, '16'),
                     Commit(repo, '10'), Commit(repo, '9'), Commit(repo, '8')])
 
-    def it_mine_base_mreleases(self, mreleases: SReleaseSet[MainRelease]):
+    def it_mine_base_mreleases(self):
+        mreleases = self.project.main_releases
         assert not mreleases['0.9.0'].base_mreleases.names
         assert mreleases['1.0.0'].base_mreleases.names == set(['0.9.0'])
         assert mreleases['1.1.0'].base_mreleases.names == set(['0.9.0'])
         assert mreleases['2.0.0'].base_mreleases.names == set(
             ['0.9.0', '1.0.0', '1.1.0'])
 
-    def it_mine_main_base_mrelease(self, mreleases: SReleaseSet[MainRelease]):
+    def it_mine_main_base_mrelease(self):
+        mreleases = self.project.main_releases
         assert not mreleases['0.9.0'].base_mrelease
         assert mreleases['1.0.0'].base_mrelease.name == '0.9.0'
         assert mreleases['1.1.0'].base_mrelease.name == '0.9.0'
         assert mreleases['2.0.0'].base_mrelease.name == '1.1.0'
 
-    def it_mine_main_release_time(self, mreleases: SReleaseSet[MainRelease]):
+    def it_mine_main_release_time(self):
+        mreleases = self.project.main_releases
         assert mreleases['0.9.0'].time \
             == MockRepository.ref_dt + timedelta(days=1)
         assert mreleases['1.0.0'].time \
@@ -101,24 +194,37 @@ class describe_release_miner:
         assert mreleases['2.0.0'].time \
             == MockRepository.ref_dt + timedelta(days=14)
 
-    def it_mine_main_release_cycle(self, mreleases: SReleaseSet[MainRelease]):
+    def it_mine_main_release_cycle(self):
+        mreleases = self.project.main_releases
         assert mreleases['0.9.0'].cycle == None
         assert mreleases['1.0.0'].cycle == timedelta(days=2)
         assert mreleases['1.1.0'].cycle == timedelta(days=5)
         assert mreleases['2.0.0'].cycle == timedelta(days=8)
 
-    def it_mine_main_release_delay(self, mreleases: SReleaseSet[MainRelease]):
+    def it_mine_main_release_delay(self):
+        mreleases = self.project.main_releases
         assert mreleases['0.9.0'].delay == timedelta(0)
         assert mreleases['1.0.0'].delay == timedelta(0)
         assert mreleases['1.1.0'].delay == timedelta(0)
         assert mreleases['2.0.0'].delay == timedelta(0)
 
+
 class describe_semantic_release_set:
-    def it_has_commits(self, project: Project):
-        assert project.main_releases.commits().ids \
+    @pytest.fixture(autouse=True)
+    def project(self) -> Project:
+        project = releasy.Miner(MockRepository()).apply(
+            FinalReleaseMiner(),
+            MixedHistoryCommitMiner(),
+            BaseReleaseMiner(),
+            SemanticReleaseMiner()
+        ).mine()
+        self.project = project
+
+    def it_has_commits(self):
+        assert self.project.main_releases.commits().ids \
             == set(['15', '14', '12', '11', '2', '1', '0',
                     '10', '9', '8', '6', '5', '3'])
-        assert project.patches.commits().ids \
+        assert self.project.patches.commits().ids \
             == set(['20', '19', '17', '14', '13', '12', '11', '2', '10', '9',
                     '8', '7', '4', '18', '16'])
             
