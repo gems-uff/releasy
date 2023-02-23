ffb163bec477035bff7544a30df974aabd6225db
Felipe Curty <felipecrp@gmail.com>
2023-02-23 15:59:22 -0300

    Add docstring to test

--- tests/test_release.py
+++ tests/test_release.py
@@ -6,6 +6,7 @@ from releasy.release import Release
 class describe_release_version:
 
     def it_normalizes_version_numbers(self):
+        """normalizes version numbers"""
         v1 = Release(None, "1.2.3", None).version
         assert v1.normalize(1) == [1]
         assert v1.normalize(2) == [1, 2]
@@ -14,6 +15,7 @@ class describe_release_version:
 
 
     def it_calculates_diff_vector(self):
+        """calculates diff"""
         v1 = Release(None, "1.0.0", None).version
         v2 = Release(None, "1.0.1", None).version
         result = v1.diff(v2)
